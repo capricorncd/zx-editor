6cb4da5e2a19660b44e255a585db990ac27a7f35
zx1984 <1355952333@qq.com>
2018-09-15 22:05:19 +0800

    修改编译环境banner添加及版本替换方法

--- build/banner.js
+++ build/banner.js
@@ -5,11 +5,8 @@
 const util = require('./util')
 const pkg = require('../package.json');
 
-module.exports = `/**
- * ZxEditor v${pkg.version}
- * ${pkg.homepage}
- * Copyright © 2018-present, ${pkg.author}
- * Released under the ${pkg.license} License
- * Released on: ${util.formatDate('yyyy-MM-dd hh:mm:ss')}
- */
- `
+module.exports = `${pkg.name} v${pkg.version}
+${pkg.homepage}
+Copyright © 2017-present, ${pkg.author}
+Released under the ${pkg.license} License
+Released on: ${util.formatDate('yyyy-MM-dd hh:mm:ss')}`
