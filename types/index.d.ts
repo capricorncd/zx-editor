658050eb70b745a59036b570191b7ed3e9e2969a
capricorncd <capricorncd@qq.com>
2022-08-14 16:22:49 +0900

    chore: update(master)

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,8 @@
  * Date: 2022/07/24 13:44:33 (GMT+0900)
  */
 export type * from '../packages/event-emitter/src'
-export type * from '../packages/editor'
+export type * from '../packages/editor/src'
+export type * from '../packages/helper/src'
+export type * from '../packages/style-panel/src'
+export type * from '../packages/toolbar/src'
+export type * from '../packages/zx-editor/src'
